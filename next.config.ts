5b1cdf292c11b79e04b93869a9585444c2fbc8f1
ashiransari <ansariashir35@gmail.com>
2025-09-23 21:01:28 +0530

    image fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+          port: '',
+          pathname: '/dfsbtqsxh/image/upload/**',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
